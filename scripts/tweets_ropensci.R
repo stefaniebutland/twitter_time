66daae777ca7dd8c40428df319ff79269eddedac
stefaniebutland <stefaniebutland@gmail.com>
2019-02-27 20:39:46 -0800

    plot by date

--- scripts/tweets_ropensci.R
+++ scripts/tweets_ropensci.R
@@ -3,7 +3,8 @@ library(tidyverse)
 library(here)
 library(usethis)
 library(visdat)
-library(ggbeeswarm)
+# library(ggbeeswarm)
+library(lubridate)
 
 usethis::use_github()
 
@@ -18,23 +19,23 @@ metrics <- read_csv(here("data", "twitter_metrics_monthly_rOpenSci.csv"))
 View(metrics)
 
 # use visdat pakage to visualize missing data
-vis_dat(metrics)
+# vis_dat(metrics)
 
 # ggplot2 line plot of tweets vs mentions, year vs mentions or new followers
-ggplot(data = metrics, aes(x=year, y=mentions)) +
-  geom_point()
-
-ggplot(data = metrics, aes(x=tweets, y=mentions)) +
-  geom_point()
-
-ggplot(data = metrics, aes(x=year, y=new_followers)) +
-  geom_point()
-
-ggplot(data = metrics, aes(x=year, y=followers)) +
-  geom_point()
-
-ggplot(data = metrics, aes(x=new_followers, y=mentions)) +
-  geom_point()
+# ggplot(data = metrics, aes(x=year, y=mentions)) +
+#   geom_point()
+# 
+# ggplot(data = metrics, aes(x=tweets, y=mentions)) +
+#   geom_point()
+# 
+# ggplot(data = metrics, aes(x=year, y=new_followers)) +
+#   geom_point()
+# 
+# ggplot(data = metrics, aes(x=year, y=followers)) +
+#   geom_point()
+# 
+# ggplot(data = metrics, aes(x=new_followers, y=mentions)) +
+#   geom_point()
 
 
 
@@ -49,28 +50,49 @@ glimpse(metrics)
 
 
 # create new column by uniting 2 other columns using unite
-metrics %>% unite(month_year, month:year, remove = FALSE)
+metrics_bymonth <- metrics %>% unite(month_year, month:year, remove = FALSE)
+
+# plot a twitter metric with x = month_year
+# problem is that x-axis is re-ordered alphabetically
+ggplot(data = metrics_bymonth, aes(x=month_year, y=tweet_impressions/1000)) +
+  geom_point()
+
+ggplot(data = metrics_bymonth, aes(x=month_year, y=followers)) +
+  geom_point()
+
+# this still groups x axis points strangely
+ggplot(data = metrics, aes(x=commit_date, y=followers)) +
+  geom_point()
+
+ggplot(data = metrics, aes(x=37:1, y=followers)) +
+  geom_point()
+
+# use lubridate to make date and plot following https://r4ds.had.co.nz/dates-and-times.html
+# create new column with commit date in proper format. This worked!!!
+# next, need to look at the actual dates in raw data and see which ones I can label with approx real dates rather than GitHub commit dates that give multiple y-axis points for one GitHub commit date
+test <- metrics %>% 
+  mutate (date = ymd(commit_date))
+
+ggplot(data = test, aes(x=date, y=followers)) +
+  geom_point()
 
 # calculate a new column using mutate
 # this example gets difference in number of followers each month
-followersgrowth <- metrics %>% mutate(followersdiff = followers - lag(followers))
+# followersgrowth <- metrics %>% mutate(followersdiff = followers - lag(followers))
 
 # count number of observations in each year; expect 12
-metrics %>%
-  group_by(year) %>%
-  summarise(obs = n())
-
-# try jitter instead of point
-# in this case of only 12 obs per year, jitter isn't needed
-ggplot(data = metrics, aes(x=year, y=mentions)) +
-  geom_jitter()
-
-ggplot(data = metrics, aes(x=year, y=mentions)) +
-  geom_jitter()
+# metrics %>%
+#   group_by(year) %>%
+#   summarise(obs = n())
+# 
+# # try jitter instead of point
+# # in this case of only 12 obs per year, jitter isn't needed
+# ggplot(data = metrics, aes(x=year, y=mentions)) +
+#   geom_jitter()
 
 # trying beeswarm, but again, not needed for this data
-ggplot(data = metrics, aes(x=year, y=mentions)) +
-  geom_beeswarm()
+# ggplot(data = metrics, aes(x=year, y=mentions)) +
+#  geom_beeswarm()
 
 # left off at https://rladiessydney.org/post/2018/12/20/vizwhiz-1/ "1.2 Using colour to plot bug levels by site"
 
