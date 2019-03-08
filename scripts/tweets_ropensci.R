028c999f539872a1a5d5014741f3d9d2b8bcb4e4
stefaniebutland <stefaniebutland@gmail.com>
2019-03-08 08:38:24 -0800

    accomplished!

--- scripts/tweets_ropensci.R
+++ scripts/tweets_ropensci.R
@@ -32,33 +32,37 @@ View(metrics)
 # use visdat pakage to visualize missing data
 vis_dat(metrics)
 
-# ggplot2 line plot of tweets vs mentions, year vs mentions or new followers
-# ggplot(data = metrics, aes(x=year, y=mentions)) +
-#   geom_point()
-# 
+# ggplot2 line plot of tweets vs mentions, date vs mentions or new followers
+
+# followers column is the only one that is not 100% objectve, since it is not archived by twitter analytics the way mentions etc are
+ggplot(data = metrics, aes(x=date, y=followers)) +
+  geom_point()
 
 ggplot(data = metrics, aes(x=date, y=followers)) +
-   geom_point()
+   geom_line()
 
+ggplot(data = metrics, aes(x=date, y=new_followers)) +
+  geom_line()
 
-# ggplot(data = metrics, aes(x=tweets, y=mentions)) +
-#   geom_point()
-# 
-# ggplot(data = metrics, aes(x=year, y=new_followers)) +
-#   geom_point()
-# 
-# ggplot(data = metrics, aes(x=year, y=followers)) +
-#   geom_point()
-# 
-# ggplot(data = metrics, aes(x=new_followers, y=mentions)) +
-#   geom_point()
+ggplot(data = metrics, aes(x=date, y=tweets)) +
+  geom_line()
 
+ggplot(data = metrics, aes(x=date, y=tweet_impressions)) +
+  geom_line()
 
+ggplot(data = metrics, aes(x=date, y=profile_visits)) +
+  geom_line() 
+
+ggplot(data = metrics, aes(x=date, y=mentions)) +
+  geom_line()
+
+ggplot(data = metrics, aes(x=tweets, y=mentions)) +
+  geom_line()
 
 #ggplot2 plot of 37 rows of data (= monthly download) with oldest date first vs y
 # ideally would do this with month/year labelled on x-axis
-ggplot(data = metrics, aes(x=37:1, y=mentions)) +
-  geom_point()
+# ggplot(data = metrics, aes(x=37:1, y=mentions)) +
+#  geom_point()
 
 # glimpse(metrics)
 
